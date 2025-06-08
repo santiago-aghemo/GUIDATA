b41a6e0afcdba6d44278202b719959680d511f6b
= <=>
2025-06-08 17:16:27 -0300

    WOOOOO SE PUEDO HACER QUE GUARDE DATOS EN EL ARCHIVO
    ahi puse que cada vez q se inicie una "sesion" nueva, se crea un archivo con la fecha. luego va cargando cada dato acompañado por la fecha. tambien hice q si se comenzo la grabacion y se intenta grabar de vuelta, te dice q ya se inicio una grabacion

--- main.py
+++ main.py
@@ -15,14 +15,18 @@
 
 
 import tkinter as tk #tkinter, las ventanas
+from tkinter import messagebox
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg #esto es para convertir lo de matplot a tkinter
 import matplotlib.pyplot as plt #funciones para plotear
 import numpy as np #numpy, para tener funciones matematicas
 import time
 import serial #leer monitor serie
 
+
 class GUI:
     def __init__(self,port='COM3', baudrate=9600):
+        self.grabarStatus = 0
+        self.nombreArchivo = None
         self.root = tk.Tk() #este es la base de todo, sobre esta ventana van a estar todos los componentes
         #self.root.geometry('1000x1000')
         self.ser = serial.Serial(port=port, baudrate=baudrate)
@@ -40,7 +44,13 @@ class GUI:
         self.l2.grid(row=0,column=1,sticky='nsew')
         self.l2T=tk.Label(self.dataGRID,text="DATO ACTUAL")
         self.l2T.grid(row=0,column=1, sticky='n')
+
+        #vamos a hacer los botones para iniciar/parar la grabacion de datos
+        self.bR=tk.Button(self.root, text="GRABAR", font=('Arial', 16), command=lambda: self.iniciar_grabar(f"session_{time.strftime('%Y-%m-%d_%H-%M-%S')}.txt", time.strftime('%Y-%m-%d_%H-%M-%S')))
+        self.bR.pack(anchor='sw')
         
+        self.bS=tk.Button(self.root, text="STOP", font=('Arial', 16), command=self.parar_grabacion)
+        self.bS.pack(anchor='sw')
         self.xdata, self.yadata = [],[] #iniciamos los datos de x e y ambos en cero
         self.counter=0
 
@@ -62,6 +72,7 @@ class GUI:
         #actualizar grafico
         valueRAW=self.ser.readline().decode().strip()#leer monitor serie
         valorF=float(valueRAW)#pasar valores del monitor serie a float
+        valueTIME=str(time.strftime('%Y-%m-%d_%H-%M-%S'))#registramos cuando llego el valor
 
         self.xdata.append(self.counter)#agregamos a los valores de x el segundo actual(counter)
         self.yadata.append(valorF)#agregamos el ultimo valor leido del monitor serie a los valores de y
@@ -89,10 +100,23 @@ class GUI:
 
         #actualizar el valor mostrado en vivo
         self.l2.config(text=str(valueRAW))
-
-        #self.dataGRID.pack(fill='both', expand=True)
-       
+        if(self.grabarStatus):#chequear si se inicio grabacion
+            self.grabar(valueRAW,valueTIME)#grabar dato
+        
         self.root.after(1000,self.actualizar)#repetir cada 1 segundo
 
-
+    def iniciar_grabar(self,path,time):#iniciar la grabacion y definir el nombre del archivo de la sesion actual
+        if(self.grabarStatus==0):
+            with open(path, 'w') as archivo:
+                archivo.write(time+': '+str(self.ser.readline().decode().strip())+'\n')
+            self.nombreArchivo = path
+            self.grabarStatus=1
+        else:#si ya se inicio una grabacion, alertar al usuario
+            messagebox.showinfo(message="YA SE HA INICIADO UNA GRABACION")
+    def grabar(self, value,time):#grabar dato actual
+        with open(self.nombreArchivo,'a') as archivo:
+            archivo.write(time+': '+str(value)+'\n')
+    def parar_grabacion(self):#cortar la grabacion
+        self.nombreArchivo = None
+        self.grabarStatus=0
 GUI()
